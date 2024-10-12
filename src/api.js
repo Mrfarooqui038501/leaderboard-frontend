bfa8e7559a1db700e4064bba11fbe3dd5dc9bfb8
Arman <armanfaruque786@gmail.com>
2024-10-12 12:41:50 +0530

    Frontend deployement

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api/users';
+const API_URL = 'https://leaderboard-backend-mp03.onrender.com/api/users';
 
 export const getUsers = () => axios.get(API_URL);
 export const addUser = (name) => axios.post(API_URL, { name });
